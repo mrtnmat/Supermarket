654fb5b18cf60ecbf9a434e7a78a0a2213ac7ba7
Matteo <47029619+mrtnmat@users.noreply.github.com>
2022-05-24 21:59:20 +0200

    Initial commit

---
+++ Supermarket/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    class Customer
+    {
+        static private int _counter = 0;
+        public readonly int Id;
+        public readonly string FirstName, LastName;
+
+        public Customer (string firstName, string lastName)
+        {
+            Id = _counter;
+            _counter++;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ Supermarket/Supermarket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket
+{
+    class Supermarket
+    {
+        readonly public string Chain;
+        readonly public string Location;
+        List<Customer> _customerList;
+
+        public Supermarket(string chain, string location)
+        {
+            Chain = chain;
+            Location = location;
+            _customerList = new List<Customer>();
+        }
+        public void InsertCustomer(Customer customer)
+        {
+            _customerList.Add(customer);
+        }
+        public IReadOnlyCollection<Customer> CustomerList => _customerList.AsReadOnly();
+    }
+}
